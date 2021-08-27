0becb99dd82b93491cf47f5a78fe57eeb3faec3e
Lucas Desouza <lucasfds2777@gmail.com>
2021-08-26 21:09:49 -0500

    add in user

--- site-modules/debian_base/manifests/init.pp
+++ site-modules/debian_base/manifests/init.pp
@@ -1,12 +1,21 @@
 class debian_base {
-	exec { update:
-		command => '/usr/bin/apt update && /usr/bin/apt upgrade -y'
-	}
-	
-	$packages = ['fish','vim','neofetch','git','net-tools','curl','golang-go']
-	
-	package { $packages:
-		ensure => 'installed',
-		require => Exec[update],
-	}
+  exec { update:
+    command => '/usr/bin/apt update && /usr/bin/apt upgrade -y'
+  }
+  
+  $packages = ['fish','vim','neofetch','git','net-tools','curl','golang-go']
+  
+  package { $packages:
+    ensure => 'installed',
+    require => Exec[update],
+  }
+
+  user { 'lucas':
+    ensure => 'present',
+    gid     => '1001',
+    groups  => ['sudo'],
+    home    => '/home/lucas',
+    shell   => '/usr/bin/fish',
+    uid     => '1001',
+  }
 }
